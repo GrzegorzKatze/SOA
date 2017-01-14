0645dd45bdd665cbaa33b8c05d3de7f2119f652a
Kot, Grzegorz <grzegorz.kot@dieboldnixdorf.com>
2017-01-14 13:49:29 +0100

    Get methods in all controllers has been created correctly #6

---
+++ Project/WebApi/WebApi/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class OrderController : ApiController
+    {
+        private WebApiEntities _dbContext;
+
+        public WebApiEntities DBContext
+        {
+            get
+            {
+                _dbContext = _dbContext == null ? new WebApiEntities() : _dbContext;
+
+                return _dbContext;
+            }
+        }
+
+        public IEnumerable<Models.Order> GetAllOrders()
+        {
+            return DBContext.Orders;
+        }
+
+        public Models.Order GetOrders(int id)
+        {
+            return DBContext.Orders.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
---
+++ Project/WebApi/WebApi/Controllers/ReceiptController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using WebApi.Models;
+
+namespace WebApi.Controllers
+{
+    public class ReceiptController : ApiController
+    {
+        private WebApiEntities _dbContext;
+
+        public WebApiEntities DBContext
+        {
+            get
+            {
+                _dbContext = _dbContext == null ? new WebApiEntities() : _dbContext;
+
+                return _dbContext;
+            }
+        }
+
+        public IEnumerable<Models.Receipt> GetAllReceipts()
+        {
+            return DBContext.Receipts;
+        }
+
+        [HttpGet]
+        [ActionName("GetReceiptById")]
+        public Models.Receipt GetReceipt(int id)
+        {
+            return DBContext.Receipts.FirstOrDefault(x => x.Id == id);
+        }
+
+        [HttpGet]
+        [ActionName("GetReceiptBySymbol")]
+        public Models.Receipt GetReceipt(string symbol)
+        {
+            return DBContext.Receipts.FirstOrDefault(x => x.Symbol == symbol);
+        }
+    }
+}
